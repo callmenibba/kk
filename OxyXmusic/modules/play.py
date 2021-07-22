20e2832e24823e476f35a41f801aa355b7ef2aa1
callmenibba <84298992+callmenibba@users.noreply.github.com>
2021-07-22 00:07:04 -0700

    Update play.py

--- OxyXmusic/modules/play.py
+++ OxyXmusic/modules/play.py
@@ -451,7 +451,7 @@ async def m_cb(b, cb):
             await cb.answer("Chat is not connected!", show_alert=True)
 
 
-@Client.on_message(command("play") & other_filters)
+@Client.on_message(command("ytt") & other_filters)
 async def play(_, message: Message):
     global que
     global useer
